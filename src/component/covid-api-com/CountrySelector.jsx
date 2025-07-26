5c531efee93965e2e2fe4a16d787cf07c31e06b4
PhucVu161 <phucvuhuu1601@gmail.com>
2025-07-26 16:12:20 +0700

    add contry selector by api reports

--- src/component/covid-api-com/CountrySelector.jsx
+++ src/component/covid-api-com/CountrySelector.jsx
@@ -1,14 +1,51 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
 export default function CountrySelector() {
+  const [countries, setCountries] = useState([]);
+  const [countrySelector, setCountrySelector] = useState("VNM");
+  useEffect(() => {
+    fetch("https://covid-api.com/api/reports")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountries(
+          Array.from(
+            new Map( //lọc các dữ liệu của province khác nhưng trùng region chỉ lấy region riêng biệt
+              data.data.map((item) => [
+                item.region.iso,
+                { iso: item.region.iso, name: item.region.name },
+              ])
+            ).values()
+          )
+        );
+      });
+  }, []);
+
   return (
     <>
-    <label htmlFor="countries">Quốc gia</label><br />
-    <select name="countries" id="countries">
-      <option value="">Việt Nam</option>
-      <option value="">Trung Quốc</option>
-    </select>
-    <div>Lựa chọn quốc gia</div>
+      <label className="text-gray-500 text-sm" htmlFor="country-selector">
+        Quốc gia
+      </label>
+      <br />
+      <select
+        value={countrySelector}
+        onChange={(e) => setCountrySelector(e.target.value)}
+        className="text-xl w-100 border-b-2"
+        name="country-selector"
+        id="country-selector"
+      >
+        {countries.length === 0 ? (
+          <option>Đang tải quốc gia...</option>
+        ) : (
+          countries.map((country) => (
+            <option key={country.iso} value={country.iso}>
+              {country.name}
+            </option>
+          ))
+        )}
+      </select>
+        <div className="text-sm text-gray-400 mt-0.5">
+          {countries.length === 0 ? "Đang tải danh sách quốc gia..." : "Lựa chọn quốc gia " + countries.length}
+        </div>
     </>
-  )
+  );
 }
